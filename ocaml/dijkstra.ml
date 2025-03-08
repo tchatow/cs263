5839b7da89cca2c2f1f237614b0202987ad97aa9
Josh A <34893923+J-avery32@users.noreply.github.com>
2025-03-07 17:59:00 -0800

    Add files via upload

--- ocaml/dijkstra.ml
+++ ocaml/dijkstra.ml
@@ -38,8 +38,45 @@ let first10000 = take_first_n (List.tl csvfile) 10000
 let graph = List.fold_left add_edge empty_graph first10000
 
 
-let dijkstra_inner (graph: nodel IntMap.t) start_n end_n unvisited distance =
-  let smallest = IntSet.fold
+let find_min_from_set unvisited distance =
+  IntSet.fold (fun curr_node smallest_node -> 
+    if ((IntMap.find curr_node distance) < (IntMap.find smallest_node distance)) then curr_node else smallest_node)
+    unvisited
+    (IntSet.min_elt unvisited)
+
+let rec dijkstra_inner (graph: nodel IntMap.t) start_n end_n unvisited distance prev =
+  (* if it's empty return distance *)
+  if IntSet.is_empty unvisited then (distance, prev) 
+  else 
+    (* If we have min distance to end node or all are unreachable then return *)
+    let min_node = find_min_from_set unvisited distance in
+    (* if min_node == end_n || (IntMap.find min_node distance) = infinity then distance 
+    else *)
+    let min_node_dist = IntMap.find min_node distance in
+
+    let update_step = 
+      (fun (distance_map, prev) edge -> 
+      let curr_dist = IntMap.find edge.to_node distance_map in
+      if not (IntSet.mem edge.to_node unvisited) then (distance_map, prev) 
+      else if (min_node_dist +. edge.len) < curr_dist then 
+        (IntMap.add edge.to_node (min_node_dist +. edge.len) distance_map, IntMap.add edge.to_node min_node prev)
+      else  (distance_map, prev)) in
+    
+    let dist_and_prev = (distance, prev) in
+    
+    let (updated_distance, updated_prev) = (IntMap.find min_node graph).edges |> 
+    List.fold_left update_step dist_and_prev in 
+    dijkstra_inner graph start_n end_n (IntSet.remove min_node unvisited) updated_distance updated_prev
+
+
+
+let rec print_path path_map start_n end_n =
+  print_int end_n;
+  print_endline "";
+  if start_n = end_n then ()
+  else 
+    let next = IntMap.find end_n path_map in
+    print_path path_map start_n next
 
 let dijkstra (graph : nodel IntMap.t) start_n end_n  = 
   let unvisited = (IntMap.bindings graph) |> 
@@ -50,6 +87,15 @@ let dijkstra (graph : nodel IntMap.t) start_n end_n  =
     List.fold_left (fun map (node_name, _) -> IntMap.add node_name infinity map) IntMap.empty) 
     |> IntMap.add start_n 0.  in
   
+  let (_, path_map) =  dijkstra_inner graph start_n end_n unvisited distance IntMap.empty in
+  print_path path_map start_n end_n
+
+  
+
+
+let () = dijkstra graph 1 4
+  
+  
   
 
 
@@ -57,7 +103,7 @@ let dijkstra (graph : nodel IntMap.t) start_n end_n  =
 
   
 
-let () = print_int (IntMap.cardinal graph)
+(* let () = print_int (IntMap.cardinal graph) *)
 
 
 
