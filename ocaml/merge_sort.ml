c95b57a4b081660274cef1324bd2790a85e93c48
Josh A <34893923+J-avery32@users.noreply.github.com>
2025-03-09 15:55:40 -0700

    Add files via upload

--- ocaml/merge_sort.ml
+++ ocaml/merge_sort.ml
@@ -1,6 +1,6 @@
 (* open Core *)
 
-let generate_random_list n =
+ let generate_random_list n =
   let rec random_list_aux lst n =
     if n <= 0 then lst
     else random_list_aux (Random.int n :: lst) (n - 1)
@@ -9,32 +9,35 @@ let generate_random_list n =
 
 
 let rec split_list_internal lst num acc =
-  let () = assert ((List.length (List.nth lst 1)) > num - acc) in
   if acc = num then lst else
-    match lst with
-    | [] -> raise (Invalid_argument "list should not be empty")
-    | h :: t -> split_list_internal [h @ [(List.hd (List.hd t))]; (t |> List.hd |> List.tl)] num (acc+1)
+    let (left,right) = lst in
+    split_list_internal ((List.hd right)::left, List.tl right) num (acc+1)
 
 let split_list lst num = 
-  split_list_internal [[];lst] num 0
+  let (lst1, lst2) = split_list_internal ([],lst) num 0 in
+  (List.rev lst1, lst2)
 
 let rec merge lst1 lst2 = 
   match lst1 with 
   | h1 :: t1 -> 
     (match lst2 with
-    | h2 :: t2 -> if h2 > h1 then [h1] @ merge t1 ([h2] @ t2) else [h2] @ merge ([h1]@t1) t2
+    | h2 :: t2 -> if h2 > h1 then h1 :: merge t1 (lst2) else h2 :: merge (lst1) t2
     | [] -> lst1)
   | [] -> lst2
 
-let rec merge_sort lst = 
+let rec merge_sort_aux lst len = 
+  let left_len = len/2 in
+  let right_len = (Int.to_float len)/.2.0 |> Float.ceil |> Float.to_int in
   match lst with 
   | [] -> []
   | [a] -> [a]
   | [a1; a2] -> if a1 > a2 then [a2;a1] else [a1;a2]
-  | _ :: _ -> let split = split_list lst ((List.length lst)/2) in
-  merge (merge_sort (List.nth split 0)) (merge_sort (List.nth split 1)) 
+  | _ :: _ -> let (left, right) = split_list lst len in
+  merge (merge_sort_aux left left_len) (merge_sort_aux right right_len) 
 
 
+let merge_sort lst =
+  merge_sort_aux lst (List.length lst)
 
 let benchmark n =
   for i = 0 to n do
@@ -42,9 +45,8 @@ let benchmark n =
     let t1 = Core.Time_ns.now() in
     ignore(merge_sort lst);
     let t2 = Core.Time_ns.now() in
-    print_string "2^";
-    print_int i;
-    print_endline "";
+    print_int (List.length lst);
+    print_string ", ";
     print_int (Core.Time_ns.to_int_ns_since_epoch t2 - Core.Time_ns.to_int_ns_since_epoch t1);
     print_endline "";
   done
@@ -61,4 +63,19 @@ Writing every loop by using a recursive function forced me into a different mode
 that merge sort would be easier/more elegant with ocaml due to the functional/recursive nature of the language.
 However, I think that it may be more elegant to write in languages that use loops more liberally.
 *)
-     
+
+(* My original  split_list function looked like this: 
+
+let rec split_list_internal lst num acc =
+  if acc = num then lst else
+    let (left,right) = lst in
+    split_list_internal (left @ [List.hd right], List.tl right) num (acc+1)
+
+let split_list lst num = 
+  split_list_internal ([],lst) num 0
+  
+  However, after benchmarking the merge sort algorithm I realized that it was not running in
+  O(nlogn), I compared it with the top down algorithm in ocaml for merge sort here: https://gist.github.com/mrvn/82577b32b83574048ce8
+  and I realized that my split list algorithm was using the @ (append) operator which is O(n) time as opposed to the
+  :: (cons) operator which is O(1) time according to this post https://stackoverflow.com/questions/40008949/time-complexity-of-and-ocaml
+  *)
